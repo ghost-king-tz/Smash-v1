de6d6bab0c0c691549c6b748f41d44ae87c75f8c
ghost-king-tz <hi2879639@gmail.com>
2025-07-02 13:00:14 +0300

    Update settings.js.
    Mh

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'LOFTXMD',
   author: '‎',
-  botName: "*𝐒𝐌𝐀𝐒𝐇-𝐕𝟏* 💥",
-  botOwner: 'Sir Loft', // Your name
-  ownerNumber: '255778018545', //Set your number here without + symbol, just add country code & number without any space
+  botName: "*NexonForce-X* 🤫🗿",
+  botOwner: 'NexonMaster', // Your name
+  ownerNumber: '255772207147', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "NexonForce-X: The ultimate AI-powered WhatsApp bot engineered for speed, security, and supreme command control. Powered by cutting-edge tech to serve your group management and chat automation needs seamlessly.",
   version: "2.0.6",
 };
 
