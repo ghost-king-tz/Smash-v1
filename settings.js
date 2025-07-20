58c0d4286c53b8b705692293483099787d095a49
ghost-king-tz <hi2879639@gmail.com>
2025-07-20 09:13:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'LOFTXMD',
-  author: '‎',
-  botName: "*NexonForce-X* 🤫🗿",
-  botOwner: 'NexonMaster', // Your name
-  ownerNumber: '255772207147', //Set your number here without + symbol, just add country code & number without any space
+  packname: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯',
+  author: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯‎',
+  botName: "*Angel of Battle V4*",
+  botOwner: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯', // Your name
+  ownerNumber: '2557719632816', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
-  description: "NexonForce-X: The ultimate AI-powered WhatsApp bot engineered for speed, security, and supreme command control. Powered by cutting-edge tech to serve your group management and chat automation needs seamlessly.",
+  description: "Angel of Battle V4: The ultimate AI-powered WhatsApp bot engineered for speed, security, and supreme command control. Powered by cutting-edge tech to serve your group management and chat automation needs seamlessly.",
   version: "2.0.6",
 };
 
