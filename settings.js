b56dd7b15d83323135a78adb671842f4c8c8e5dc
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-08-06 15:10:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯',
-  author: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯‎',
+  packname: '𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝',
+  author: '𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝',
   botName: "*Smash-v1*",
-  botOwner: '⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯', // Your name
-  ownerNumber: '2557719632816', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝', // Your name
+  ownerNumber: '2557719632816', /Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
-  description: "Angel of Battle V4: The ultimate AI-powered WhatsApp bot engineered for speed, security, and supreme command control. Powered by cutting-edge tech to serve your group management and chat automation needs seamlessly.",
+  description: "Smash V1: The ultimate AI-powered WhatsApp bot engineered for speed, security, and supreme command control. Powered by cutting-edge tech to serve your group management and chat automation needs seamlessly.",
   version: "2.0.6",
 };
 
